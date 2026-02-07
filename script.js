072dffc8d2e96b1b2ecb8380a33e2fb0e4410a22
Aryan Ghugare <aryanghugare75@gmail.com>
2026-02-07 09:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //your JS code here. If required.
+
+const output = document.querySelector("#output");
+let input = document.querySelector("#ip");
+let startButton = document.querySelector("#btn");
+
+let console = document.querySelector("#console");
+
+
+startButton.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let inputNumber = input.value ;
+	
+	setTimeout(()=>{
+		output.innerHTML =`Result : ${inputNumber}`
+		
+		
+	},2000)
+	
+	setTimeout(()=>{
+		inputNumber = 2 * inputNumber ;
+		output.innerHTML = ``
+		output.innerHTML =`Result : ${inputNumber}`
+	},3000)
+
+	
+	setTimeout(()=>{
+		inputNumber -=3 ;
+		output.innerHTML = ``
+		output.innerHTML =`Result : ${inputNumber}`
+	},4000)
+
+	
+	setTimeout(()=>{
+		inputNumber = parseFloat(inputNumber/2) ;
+		output.innerHTML = ``
+		output.innerHTML =`Result : ${inputNumber}`
+	},5000)
+
+	setTimeout(()=>{
+		inputNumber += 10 ; 
+		output.innerHTML = ``
+		output.innerHTML =`Result : ${inputNumber}`
+	},6000)
+
+
+
+	
+	
+	
+})
+
+
+
