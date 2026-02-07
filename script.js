f1acace908ec3c29cee320c9ffdb6e6615017e98
Aryan Ghugare <aryanghugare75@gmail.com>
2026-02-07 09:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-//your JS code here. If required.
-
 const output = document.querySelector("#output");
-let input = document.querySelector("#ip");
-let startButton = document.querySelector("#btn");
+const inputEl = document.querySelector("#ip");
+const startButton = document.querySelector("#btn");
 
-let console = document.querySelector("#console");
+startButton.addEventListener("click", () => {
+  let num = Number(inputEl.value);
 
+  // First Promise (2 sec)
+  new Promise((resolve) => {
+    setTimeout(() => {
+      output.innerHTML = `Result: ${num}`;
+      resolve(num);
+    }, 2000);
+  })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          result = result * 2;
+          output.innerHTML = `Result: ${result}`;
+          resolve(result);
+        }, 2000);
+      });
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          result = result - 3;
+          output.innerHTML = `Result: ${result}`;
+          resolve(result);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          result = result / 2;
+          output.innerHTML = `Result: ${result}`;
+          resolve(result);
+        }, 1000);
+      });
+    })
+    .then((result) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          result = result + 10;
+          output.innerHTML = `Final Result: ${result}`;
+          resolve(result);
+        }, 1000);
+      });
+    })
 
-startButton.addEventListener("click",(e)=>{
-	e.preventDefault();
-	let inputNumber = input.value ;
-	
-	setTimeout(()=>{
-		output.innerHTML =`Result : ${inputNumber}`
-		
-		
-	},2000)
-	
-	setTimeout(()=>{
-		inputNumber = 2 * inputNumber ;
-		output.innerHTML = ``
-		output.innerHTML =`Result : ${inputNumber}`
-	},3000)
-
-	
-	setTimeout(()=>{
-		inputNumber -=3 ;
-		output.innerHTML = ``
-		output.innerHTML =`Result : ${inputNumber}`
-	},4000)
-
-	
-	setTimeout(()=>{
-		inputNumber = parseFloat(inputNumber/2) ;
-		output.innerHTML = ``
-		output.innerHTML =`Result : ${inputNumber}`
-	},5000)
-
-	setTimeout(()=>{
-		inputNumber += 10 ; 
-		output.innerHTML = ``
-		output.innerHTML =`Final Result : ${inputNumber}`
-	},6000)
-
-
-
-	
-	
-	
-})
-
-
-
+    .catch((error) => {
+      console.log(error);
+    });
+});
