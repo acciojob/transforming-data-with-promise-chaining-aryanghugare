ccde65f4f43973f277ba96946445477e1b8c3878
Aryan Ghugare <aryanghugare75@gmail.com>
2026-06-10 15:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
 //your JS code here. If required.
+
+const output = document.querySelector("#output") ;
+
+
+// promise1.then((data)=>output.innerHTML = `Result: ${data}`)
+function handleClick() {
+const numr = document.querySelector("#ip") ;
+function first(num) {
+	return	 new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+	// num =num.value;
+   resolve(num.value) ;
+   },2000)
+
+})
+}
+
+function two(num) {
+return	 new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+	   num = num*2
+	   resolve(num) ;
+   },2000)
+	
+})
+}
+
+function three(num) {
+	return  new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+	   num = num - 3
+	   resolve(num) ;
+   },1000)
+	
+})
+}
+
+function four(num) {
+	return  new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+	   num = num / 2 ;
+	   resolve(num) ;
+   },1000)
+	
+})
+}	
+
+function five(num) {
+	return  new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+	   num = num + 10 ;
+	   resolve(num) ;
+   },1000)
+	
+})
+}	
+
+	
+first(numr).then((data)=> { output.innerHTML = `Result: ${data}`
+		return data 				 
+	})
+.then((data)=>two(data)
+	 .then((data)=>{output.innerHTML = `Result: ${data}`
+		return data		   }))
+	.then((data)=>three(data)
+		 .then((data)=>{output.innerHTML = `Result: ${data}`
+					   return data 
+	}))
+	.then((data)=> four(data)
+	.then((data)=> {
+		output.innerHTML = `Result: ${data}`
+		return data ;
+	}))
+	.then((data)=> five(data)
+	.then((data)=> {
+		output.innerHTML = `Result: ${data}`
+		return data ;
+	}))
+
+}
